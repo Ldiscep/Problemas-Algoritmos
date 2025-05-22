f684db5ca57c43abb5da0309139372fd116b9885
Ldiscep <lucianodiscepolo@gmail.com>
2025-05-22 17:49:46 -0300

    avance tp3

--- Tp3/tp3.c
+++ Tp3/tp3.c
@@ -1,7 +1,7 @@
 #include "tp3.h"
 #include <stdio.h>
 #include <stdlib.h>
-#define C_I 17 // capacidad inicial sugerida por el copilot 😶‍🌫️
+#define C_I 170// capacidad inicial sugerida por el copilot 😶‍🌫️
 
 typedef struct {
   char *key;
@@ -15,7 +15,7 @@ struct dictionary {
   size_t cantidad;
   destroy_f destroyer;
 };
-unsigned long funcion_hash(const char *clave, int table_size) {
+unsigned long funcion_hash(const char *clave, size_t table_size) {
   unsigned long hash = 5381;
   int c;
 
@@ -23,7 +23,7 @@ unsigned long funcion_hash(const char *clave, int table_size) {
       hash = ((hash << 5) + hash) + c; // hash * 33 + c
   }
 
-  return hash % (unsigned int)table_size;
+  return hash % table_size;
 }
 
 
@@ -44,25 +44,93 @@ dictionary_t *dictionary_create(destroy_f destroy) {
 };
 
 bool dictionary_put(dictionary_t *dictionary, const char *key, void *value) {
-return false;
-};
+   unsigned long hashed_key = funcion_hash(key, dictionary->capacidad);
+   int indice = (int)hashed_key;
+  while (true){
+    if (indice > dictionary->capacidad) return false;
+    if (dictionary->tabla[indice].ocupado == true) {
+      indice ++;
+      continue;
+    }
+    dictionary->tabla[indice].key = (char *)hashed_key;
+    dictionary->tabla[indice].value= value;
+    dictionary->tabla[indice].ocupado = true;
+    dictionary->cantidad ++ ;
+    return true;
+  }
+}
 
 void *dictionary_get(dictionary_t *dictionary, const char *key, bool *err) {
+  unsigned long hashed_key =funcion_hash(key,dictionary->capacidad);
+  int indice = (int)hashed_key;
+  while (dictionary->tabla[indice].ocupado == true || dictionary->tabla[indice].borrado==true ){ 
+
+    if (hashed_key == (unsigned long)dictionary->tabla[indice].key) {
+      err = false;
+      return dictionary->tabla[indice].value;}
+    else indice++;
+
+    if (indice ==dictionary->capacidad) return false;
+  }
+  *err= true;
   return NULL;
-};
+}
+
 
 bool dictionary_delete(dictionary_t *dictionary, const char *key) {
-  return true;
+  
+  unsigned long hashed_key =funcion_hash(key,dictionary->capacidad);
+  int indice = (int)hashed_key;
+  while (dictionary->tabla[indice].ocupado == true || dictionary->tabla[indice].borrado==true ){ 
+
+    if (hashed_key == (unsigned long)dictionary->tabla[indice].key) {
+      dictionary->tabla[indice].ocupado = false;
+      dictionary->tabla[indice].borrado= true;
+      dictionary->cantidad --;
+      return true;
+    }
+    else indice++;
+    if (indice ==dictionary->capacidad) return false;
+  }
+  return false;
 };
 
 void *dictionary_pop(dictionary_t *dictionary, const char *key, bool *err) {
+  unsigned long hashed_key =funcion_hash(key,dictionary->capacidad);
+  int indice = (int)hashed_key;
+  while (dictionary->tabla[indice].ocupado == true || dictionary->tabla[indice].borrado==true ){ 
+
+    if (hashed_key == (unsigned long)dictionary->tabla[indice].key) {
+      dictionary->tabla[indice].ocupado = false;
+      dictionary->tabla[indice].borrado= true;
+      dictionary->cantidad --;
+      * err= false;
+      return dictionary->tabla[indice].value;
+    }
+    indice++;
+    if (indice ==dictionary->capacidad) {*err=true ;return NULL;}
+  }
+  *err = true;
   return NULL;
 };
 
 bool dictionary_contains(dictionary_t *dictionary, const char *key) {
+  unsigned long hashed_key =funcion_hash(key,dictionary->capacidad);
+  int indice = (int)hashed_key;
+  while (dictionary->tabla[indice].ocupado == true || dictionary->tabla[indice].borrado==true ){ 
+
+    if (hashed_key == (unsigned long)dictionary->tabla[indice].key) {
+      return true;}
+    else indice++;
+
+    if (indice ==dictionary->capacidad) return false;
+  }
   return true;
 };
 
-size_t dictionary_size(dictionary_t *dictionary) { return 0; };
+size_t dictionary_size(dictionary_t *dictionary) {  
+  return dictionary->cantidad;
+};
+
 
 void dictionary_destroy(dictionary_t *dictionary){};
