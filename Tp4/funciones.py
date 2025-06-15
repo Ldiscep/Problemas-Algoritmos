2c064cf0476baccd94fc3998bf6059c7cf416c70
Ldiscep <lucianodiscepolo@gmail.com>
2025-06-14 21:20:13 -0300

    tp avanzado hasta el punto 5

--- Tp4/funciones.py
+++ Tp4/funciones.py
@@ -1,7 +1,6 @@
 from collections import deque
 from graph import *
-import pandas as pd
-from tqdm import tqdm
+from random import randint, choice
 def bfs(graph: Graph, start: str) -> None:
     visited = set()
     queue = deque()
@@ -76,4 +75,90 @@ def largest_connected(graph):
             if component_size > max_size:
                 max_size = component_size
 
+
+
+    
     return max_size
+
+def triangles(graph: Graph):
+    triangulos = 0
+    seen = set()
+    for v in graph._graph:
+        for u in graph.get_neighbors(v):
+            if u <= v:
+                continue 
+            for w in graph.get_neighbors(u):
+                if w <= u or w == v:
+                    continue
+                if v in graph.get_neighbors(w):
+
+                    triangle = tuple(sorted([v, u, w]))
+                    if triangle not in seen:
+                        seen.add(triangle)
+                        triangulos += 1
+    return triangulos
+
+
+def bfs_max_distance(graph, start):
+    visited = set()
+    distance = {start: 0}
+    queue = deque([start])
+    visited.add(start)
+
+    max_dist = 0
+    farthest_node = start
+
+    while queue:
+        current = queue.popleft()
+
+        for neighbor in graph.get_neighbors(current):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                distance[neighbor] = distance[current] + 1
+                if distance[neighbor] > max_dist:
+                    max_dist = distance[neighbor]
+                    farthest_node = neighbor
+                queue.append(neighbor)
+
+    return farthest_node, max_dist
+
+def graph_diameter(graph):
+    if not graph._graph:
+        return 0
+
+    start_node = next(iter(graph._graph))
+    farthest_node, _ = bfs_max_distance(graph, start_node)
+
+    _, diameter = bfs_max_distance(graph, farthest_node)
+
+    return diameter
+
+def pagerank(graph:Graph):
+    vertices=list(graph._graph)
+    #vertices=list(vertices.keys())
+    
+    dic_final={}
+    randomwalks=10000
+    for walk in range(randomwalks):
+        vtx=randint(0,len(vertices))
+
+        for paso in range(1000):
+            if paso==0:
+                encontrados= graph.get_neighbors(str(vertices[vtx]))
+            else: encontrados= graph.get_neighbors(vtx)
+            if not encontrados:
+                continue
+            vtx=choice(encontrados)
+            for vecino in encontrados:
+                if vecino in dic_final:
+                    dic_final[vecino]+=1
+                else: dic_final[vecino]=1
+                
+    1
+
+    # Ordenar dic_final por valor descendente
+    sorted_dic = sorted(dic_final.items(), key=lambda x: x[1], reverse=True)
+    return sorted_dic
+
+
+        
